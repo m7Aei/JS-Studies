45218e6b9cdc7a0562305abc99027c2c923c05ab
m7Aei <mateuspm2015@hotmail.com>
2019-08-30 11:45:01 -0300

    Criando o método de ordenação burble sort

---
+++ ordinationMethods/methods/burble.js
@@ -0,0 +1,23 @@
+// método de ordenação burble
+const { numbers } = require("../data");
+
+function burbleSort(values) {
+  var origValues = values.slice();
+  var length = origValues.length - 1;
+  do {
+    var swapped = false;
+    for (var i = 0; i < length; ++i) {
+      if (origValues[i] > origValues[i + 1]) {
+        var temp = origValues[i];
+        origValues[i] = origValues[i + 1];
+        origValues[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped === true);
+  return origValues;
+}
+
+const ordinationArray = burbleSort(numbers);
+
+console.table(ordinationArray);
